3a68a0462c673498ffc38ed797d55646beea88f5
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-11-02 14:10:34 +0530

    created models

---
+++ staticData.js
@@ -0,0 +1,10 @@
+const intro = [
+  {
+    welcomeText: "Hi I'm",
+    firstName: 'Harish',
+    lastName: 'Kamuni',
+    caption: 'I build thing for web.',
+    description:
+      'I am Full Stack Web Developer. Currently I am working as a React Developer in India. Also, sharing my knowledge to the students that I have gained in my carrier through online teaching across the world.',
+  },
+];
