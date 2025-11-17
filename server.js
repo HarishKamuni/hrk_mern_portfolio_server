d8b45a2357b2dd9befee3c5c10d85d70ae850150
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-11-17 16:47:36 +0530

    no changes made

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use('/api/portfolio', portfolioRoute);
 
 const port = process.env.PORT || 5000;
 
+//Heroku configration
+// const path = require('path');
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build.index.html'));
+//   });
+// }
 
 const start = () => {
   try {
