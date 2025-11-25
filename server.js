31448e7f55bf6e41ed2c7b7be09212b08544ee75
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-11-25 18:29:06 +0530

    allow all origins

--- server.js
+++ server.js
@@ -10,26 +10,15 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://endearing-kangaroo-e2bcbf.netlify.app/',
-    ],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: 'Content-Type, Authorization',
   })
 );
 
 app.use('/api/portfolio', portfolioRoute);
 
 const port = process.env.PORT || 5000;
-
-//Heroku configration
-// const path = require('path');
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build.index.html'));
-//   });
-// }
 
 const start = () => {
   try {
